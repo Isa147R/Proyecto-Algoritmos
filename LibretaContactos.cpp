c49488e869129ada51fa22d8f3c8e92a9529f198
Isa147R <131714039+Isa147R@users.noreply.github.com>
2023-11-03 17:34:19 -0500

    Create LibretaContactos.cpp

---
+++ LibretaContactos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#inlcude <ArbolBST>
+#inlcude <VectorIndex>
+#inlcude <ColaBackup>
+
+class LibretaContactos {
+private:
+    ArbolBST arbol_bst;
+    VectorIndex vector_index;
+    ColaBackup cola_backup;
+
+public:
+    // Implementa los métodos necesarios para la libreta de contactos
+};
